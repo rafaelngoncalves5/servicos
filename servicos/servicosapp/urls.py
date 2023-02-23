a4e7837043d2923f8265fb91b7ec4bd74f6b0c73
Rafael N <rafaelngoncalves5@outlook.com>
2023-02-23 01:24:05 -0300

    Progresso views, templates e models

--- servicos/servicosapp/urls.py
+++ servicos/servicosapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'servicosapp'
 urlpatterns = [
    path('', views.index, name='index'),
+   path('servicos/', views.index_servicos, name="index_servicos"),
+   path('servicos/criar', views.criar_servicos, name="criar_servicos"),
 ]
--- servicos/servicosapp/views.py
+++ servicos/servicosapp/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from . models import Servico
 
 def index(request):
        return render(request, 'servicosapp/index.html')
+
+def index_servicos(request):
+       servicos = Servico.objects.all()
+       context = {
+              'servicos': servicos
+       }
+       return render(request, 'servicosapp/servicos/index.html', context)
+
+def criar_servicos(request):
+       return render(request, 'servicosapp/servicos/criar.html')
