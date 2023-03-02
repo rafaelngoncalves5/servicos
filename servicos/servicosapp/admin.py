4563a40d7da99ffb8a7ce2a6b540178edc5b8be7
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-02 11:11:26 -0300

    Página de administração

--- servicos/servicosapp/admin.py
+++ servicos/servicosapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Municipio, Servico
 
 # Register your models here.
+admin.site.register(Servico)
+
+class ServicosAdmin(admin.ModelAdmin):
+    list_filter = ('titulo')
